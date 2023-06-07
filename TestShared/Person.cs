270eb2e71ac7ef210470c8b86925419dcc9070be
Tim Heuer <tim@timheuer.com>
2023-06-07 07:44:36 -0700

    Added shared project and linked file

---
+++ TestShared/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestShared
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}

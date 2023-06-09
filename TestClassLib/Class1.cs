4d282f1648ae9bb1bd27211c372a8610e292f45d
Tim Heuer <tim@timheuer.com>
2023-06-09 07:29:35 -0700

    Added P2P reference

--- TestClassLib/Class1.cs
+++ TestClassLib/Class1.cs
@@ -2,5 +2,17 @@
 
 public class Class1
 {
+    public void Test1()
+    {
+        int val = 0;
+        Console.WriteLine(val);
+    }
 
+    public void Test2()
+    {
+        int val = 0;
+        Console.WriteLine(val);
+    }
+
+    public string? SomeValue { get; set; }
 }

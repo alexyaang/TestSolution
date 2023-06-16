4952343324f13570480e88d644deda2102bb9778
Tim Heuer <tim@timheuer.com>
2023-06-16 07:34:08 -0700

    Added inline test

---
+++ TestXUnit/MathTests.cs
@@ -0,0 +1,14 @@
+﻿namespace TestXUnit;
+
+public class MathTests
+{
+    [Theory]
+    [InlineData(1,2,3)]
+    [InlineData(-4,-6,-10)]
+    [InlineData(-2,2,0)]
+    public void CanAddTheory(int val1, int val2, int expected)
+    {
+        var result =  val1 + val2;
+        Assert.Equal(expected, result);
+    }
+}

3b881781a95253b0bded5eea77dfc3d302ae0f03
Tim Heuer <tim@timheuer.com>
2023-06-17 10:39:37 -0700

    Added xunit MemberData samples

--- TestXUnit/MathTests.cs
+++ TestXUnit/MathTests.cs
@@ -11,4 +11,40 @@ public class MathTests
         var result =  val1 + val2;
         Assert.Equal(expected, result);
     }
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    internal void CanAddTheoryMemberDataProperty(int val1, int val2, int expected)
+    {
+        var result = val1 + val2;
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetData), parameters: 3)]
+    internal void CanAddTheoryMemberDataMethod(int val1, int val2, int expected)
+    {
+        var result = val1 + val2;
+        Assert.Equal(expected, result);
+    }
+
+    public static IEnumerable<object[]> Data =>
+    new List<object[]>
+    {
+        new object[] { 1, 2, 3},
+        new object[] { -4, -6, -10 },
+        new object[] { -2, 2, 0 }
+    };
+
+    public static IEnumerable<object[]> GetData(int numTests)
+    {
+        var allData = new List<object[]>
+        {
+            new object[] { 1, 2, 3 },
+            new object[] { -4, -6, -10 },
+            new object[] { -2, 2, 0 },
+        };
+
+        return allData.Take(numTests);
+    }
 }
